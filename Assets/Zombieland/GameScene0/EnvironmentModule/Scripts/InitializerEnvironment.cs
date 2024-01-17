e7fb10df5a098f0e1b194499c887f6072be7aab5
we1lcarry0n <andriy.fed@gmail.com>
2024-01-17 17:17:51 +0200

    final environment commit

--- Assets/Zombieland/GameScene0/EnvironmentModule/Scripts/InitializerEnvironment.cs
+++ Assets/Zombieland/GameScene0/EnvironmentModule/Scripts/InitializerEnvironment.cs
@@ -4,12 +4,11 @@ namespace Zombieland.GameScene0.EnvironmentModule
 {
     public class InitializerEnvironment
     {
-        private const string ENVIRONMENT_PREFAB_NAME = "Level";
+        private const string ENVIRONMENT_PREFAB_NAME = "Level0";
 
         public void Init()
         {
             GameObject prefab = Resources.Load<GameObject>(ENVIRONMENT_PREFAB_NAME);
-            Debug.Log(prefab == null);
             GameObject.Instantiate(prefab, Vector3.zero, Quaternion.identity);
         }
     }
