81f3a6c02daf1ad76cafae7d66a933229d3858e3
Vitaliy Morhun <droidovr@gmail.com>
2024-01-30 18:01:58 +0200

    Working with ragdoll.

--- Assets/Zombieland/GameScene0/CharacterModule/AnimationModule/Scripts/Test/TestRagdoll.cs
+++ Assets/Zombieland/GameScene0/CharacterModule/AnimationModule/Scripts/Test/TestRagdoll.cs
@@ -9,6 +9,7 @@ namespace Zombieland.GameScene0.CharacterModule.AnimationModule
         [SerializeField] private bool _isRagdoll;
 
         private Animator _animator;
+        private UnityEngine.CharacterController _unityCharacterController;
         private List<Rigidbody> _rigidbodies;
 
 
@@ -27,12 +28,14 @@ namespace Zombieland.GameScene0.CharacterModule.AnimationModule
         public void Init()
         {
             _animator = GetComponent<Animator>();
+            _unityCharacterController = GetComponent<UnityEngine.CharacterController>();
             _rigidbodies = new List<Rigidbody>(GetComponentsInChildren<Rigidbody>());
         }
 
         private void EnableRagdoll()
         {
             _animator.enabled = false;
+            _unityCharacterController.enabled = false;
 
             foreach (Rigidbody rigidbody in _rigidbodies)
             {
@@ -47,6 +50,7 @@ namespace Zombieland.GameScene0.CharacterModule.AnimationModule
                 rigidbody.isKinematic = true;
             }
 
+            _unityCharacterController.enabled = true;
             _animator.enabled = true;
         }
     }
