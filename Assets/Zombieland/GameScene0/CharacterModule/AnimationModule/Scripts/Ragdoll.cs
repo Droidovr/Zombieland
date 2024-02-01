f87439c1597708ede4cc6d0c005fcdaae64c6863
droidovr <droidovr@gmail.com>
2024-02-01 07:43:26 +0200

    Working with ragdoll.

--- Assets/Zombieland/GameScene0/CharacterModule/AnimationModule/Scripts/Ragdoll.cs
+++ Assets/Zombieland/GameScene0/CharacterModule/AnimationModule/Scripts/Ragdoll.cs
@@ -23,32 +23,30 @@ namespace Zombieland.GameScene0.CharacterModule.AnimationModule
             _rigidbodies = new List<Rigidbody>(gameObject.GetComponentsInChildren<Rigidbody>());
 
             _parent = gameObject.transform;
-            
+
             _hipsBone = _animator.GetBoneTransform(HumanBodyBones.Hips);
 
-            Debug.Log("_hipsBone: " + _hipsBone.position);
-
-            Disable();
+            RagdollHandler(true);
         }
 
         public async void Hit(Vector3 force, Vector3 hitPosition)
         {
-            Enable();
+            RagdollHandler(false);
 
             Rigidbody injuredRigidbody = _rigidbodies.OrderBy(rigidbody => Vector3.Distance(rigidbody.position, hitPosition)).First();
             injuredRigidbody.AddForceAtPosition(force, hitPosition, ForceMode.Impulse);
 
             //await Task.Delay(300);
 
-            //Disable();
+            //RagdollHandler(true);
         }
 
         public void StandUp()
         {
-            Disable();
+            RagdollHandler(true);
 
-            AdjustParentRorationToHipsBone();
             AdjustParentPositionToHipsBone();
+            AdjustParentRorationToHipsBone();
 
 
             if (IsFrontUp())
@@ -61,34 +59,24 @@ namespace Zombieland.GameScene0.CharacterModule.AnimationModule
             }
         }
 
-        private void Enable()
+        private void RagdollHandler(bool isDisabled)
         {
-            _animator.enabled = false;
-            _unityCharacterController.enabled = false;
+            _unityCharacterController.enabled = isDisabled;
+            _animator.enabled = isDisabled;
 
             foreach (Rigidbody rigidbody in _rigidbodies)
             {
-                rigidbody.isKinematic = false;
+                rigidbody.isKinematic = isDisabled;
             }
-        }
-
-        private void Disable()
-        {
-            foreach (Rigidbody rigidbody in _rigidbodies)
-            {
-                rigidbody.isKinematic = true;
-            }
-
-            _unityCharacterController.enabled = true;
-            _animator.enabled = true;
         }
 
         private void AdjustParentPositionToHipsBone()
-        { 
+        {
             Vector3 initHipsPosition = _hipsBone.position;
             _parent.position = initHipsPosition;
 
-            if (Physics.Raycast(_parent.position, Vector3.down, out RaycastHit hit, 5, 1 << LayerMask.NameToLayer("Ground")))
+            RaycastHit hit;
+            if (Physics.Raycast(_parent.position, Vector3.down, out hit, Mathf.Infinity))
             {
                 _parent.position = new Vector3(_parent.position.x, hit.point.y, _parent.position.z);
             }
@@ -101,7 +89,12 @@ namespace Zombieland.GameScene0.CharacterModule.AnimationModule
             Vector3 initHipsPosition = _hipsBone.position;
             Quaternion initHipsRotation = _hipsBone.rotation;
 
-            Vector3 directionForRotate = -_hipsBone.up;
+            Vector3 directionForRotate = _hipsBone.up;
+
+            if (IsFrontUp())
+            {
+                directionForRotate *= -1;
+            }
 
             directionForRotate.y = 0;
 
@@ -113,7 +106,7 @@ namespace Zombieland.GameScene0.CharacterModule.AnimationModule
         }
 
         private bool IsFrontUp()
-        { 
+        {
             return Vector3.Dot(_hipsBone.up, Vector3.up) > 0;
         }
     }
