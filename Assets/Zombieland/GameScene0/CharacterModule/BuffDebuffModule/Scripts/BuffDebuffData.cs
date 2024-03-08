1c6116be6fd6dbda92115069962056359cf6ae8d
Droidovr <droidovr@gmail.com>
2024-03-08 11:39:20 +0200

    Fixed Code on Kirill's recommendation.

--- Assets/Zombieland/GameScene0/CharacterModule/BuffDebuffModule/Scripts/BuffDebuffData.cs
+++ Assets/Zombieland/GameScene0/CharacterModule/BuffDebuffModule/Scripts/BuffDebuffData.cs
@@ -2,13 +2,13 @@ namespace Zombieland.GameScene0.CharacterModule.BuffDebuffModule
 {
     public class BuffDebuffData
     {
-        public string ID { get; private set; } // Serializable
-        public string Name { get; private set; } // Serializable
-        public string IconID { get; private set; } // Serializable
-        public string PreabID { get; private set; } // Serializable
-        public VFXPosition VFXPosition { get; private set;} // Serializable
-        public float LifeTime { get; private set; } // Serializable
-        public float Interval { get; private set; } // Serializable
+        public string ID { get; set; } // Serializable
+        public string Name { get; set; } // Serializable
+        public string IconID { get; set; } // Serializable
+        public string PreabID { get; set; } // Serializable
+        public VFXPosition VFXPosition { get; set;} // Serializable
+        public float LifeTime { get; set; } // Serializable
+        public float Interval { get; set; } // Serializable
         public DirectImpactData DirectImpactData { get; set; }
         public ICharacterController ImpactTarget { get; set; }
         public ICharacterController Owner { get; set; }
