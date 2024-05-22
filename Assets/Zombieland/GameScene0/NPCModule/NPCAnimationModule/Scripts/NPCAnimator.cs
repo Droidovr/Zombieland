249448d93cfa598a535d345d717867c7d2a76366
droidovr <droidovr@gmail.com>
2024-05-22 20:44:26 +0300

    Fixed Character Animator Controller.

--- Assets/Zombieland/GameScene0/NPCModule/NPCAnimationModule/Scripts/NPCAnimator.cs
+++ Assets/Zombieland/GameScene0/NPCModule/NPCAnimationModule/Scripts/NPCAnimator.cs
@@ -98,11 +98,11 @@ namespace Zombieland.GameScene0.NPCModule.NPCAnimationModule
 
         private void AIFireHandler(bool isFire)
         {
-            if (_nPCAnimatorController.NPCController.NPCVisualBodyController.WeaponInScene != null)
-            {
+            //if (_nPCAnimatorController.NPCController.NPCVisualBodyController.WeaponInScene != null)
+            //{
                 _animator.SetBool("IsAttack", isFire);
                 OnAnimationAttack?.Invoke(isFire);
-            }
+            //}
         }
 
 
