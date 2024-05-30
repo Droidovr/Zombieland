1a48a59679a482c2136aee2cc22f290db8abdce4
droidovr <droidovr@gmail.com>
2024-05-30 20:13:43 +0300

    0.0-GlobalSoundSystem - add sound in Scene Level1.

---
+++ Assets/Zombieland/GameScene0/EnvironmentModule/Scripts/HelperScripts/GameobjectInScene.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+
+namespace Zombieland.GameScene0.EnvironmentModule
+{
+    public class GameobjectInScene : MonoBehaviour
+    {
+        public GameObject Phone;
+        public GameObject MineWithFire;
+        public GameObject Creamatorium;
+        public GameObject Printer;
+        public GameObject Server1;
+        public GameObject Server2;
+        public GameObject TualetMan;
+        public GameObject TualetWoman;
+        public GameObject ShowerRoomMan;
+        public GameObject ShowerRoomWoman;
+    }
+}
